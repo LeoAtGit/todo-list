fa8e0cea42d4fbf52a2815d18c4e360c01babe2e
Leo Sperling <leosperling97@gmail.com>
2016-07-23 12:24:01 +0200

    Deleted nginx-specific file, made a small skeleton

--- html/index.php
+++ html/index.php
@@ -1,15 +1,25 @@
 <!doctype html>
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="Simple TODO-List">
+	
 	<title>TODOs</title>
 	<link rel="shortcut icon" type="image/png" href="img/favicon.png?v=1"/>
+	
+	<link rel="stylesheet" href="css/public.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+	<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous"> -->
 </head>
 
 <body>
-<?php
-echo "test";
-?>
+<div class="content">
+	<h1 align="center" class="header">TODO-List</h1>
+	<?php
+	
+	//TODO
+	
+	?>
+</div>
 </body>
 
