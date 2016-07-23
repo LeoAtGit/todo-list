fc5c096a9af32c529173831e6f68bed6d3b32046
Leo Sperling <leosperling97@gmail.com>
2016-07-23 11:43:37 +0200

    Initial Commit

---
+++ html/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="Simple TODO-List">
+	<title>TODOs</title>
+	<link rel="shortcut icon" type="image/png" href="img/favicon.png?v=1"/>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+</head>
+
+<body>
+<?php
+echo "test";
+?>
+</body>
+
